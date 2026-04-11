e58646386acf7ad89a97a34d5cf15bf6286d25cc
Faysal-Maxamed <pheymoha17@gmail.com>
2026-04-11 14:25:22 +0300

    ....

--- frontend/api/axios.ts
+++ frontend/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "property-management-system-production-e024.up.railway.app/api",
   headers: {
     "Content-Type": "application/json",
   },
